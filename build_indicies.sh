ba53f482172cf0c9cb9601c3ea6ffe96dfb1aea4
Cyrus Tau <7599371+cyrustau21@users.noreply.github.com>
2025-10-30 16:35:08 -0400

    Add files via upload
    added cd40l

--- build_indicies.sh
+++ build_indicies.sh
@@ -9,6 +9,7 @@ cat hg38.knownGene.gtf NewReporterFull.gtf > hg38_NewTS.gtf
 cat hg38.knownGene.gtf ITGB1.gtf > hg38_ITGB1.gtf
 cat hg38.knownGene.gtf SMARCA4.gtf > hg38_SMARCA4.gtf
 cat hg38.knownGene.gtf TFRC.gtf > hg38_TFRC.gtf
+cat hg38.knownGene.gtf CD40L.gtf > hg38_CD40L.gtf
 
 echo "Building Reporter Index"
 STAR --runThreadN 8 \
@@ -49,4 +50,12 @@ STAR --runThreadN 8 \
 --genomeDir ~/star_index/tfrcindex \
 --genomeFastaFiles ~/star_index/refs/hg38.fa ~/star_index/refs/TFRC_trans.fa \
 --sjdbGTFfile ~/star_index/refs/hg38_TFRC.gtf \
+--sjdbOverhang 120
+
+echo "Building CD40L Index"
+STAR --runThreadN 8 \
+--runMode genomeGenerate \
+--genomeDir ~/star_index/cd40lindex \
+--genomeFastaFiles ~/star_index/refs/hg38.fa ~/star_index/refs/CD40LCis.fa ~/star_index/refs/CD40LTrans.fa \
+--sjdbGTFfile ~/star_index/refs/hg38_CD40L.gtf \
 --sjdbOverhang 120
